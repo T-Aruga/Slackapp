d7d7543eff7c757e59779a3910985021553388ee
T-Aruga <baideneo@gmail.com>
2020-04-29 14:29:36 +0900

    Add logout fuc

--- my-nuxt-app/layouts/default.vue
+++ my-nuxt-app/layouts/default.vue
@@ -5,6 +5,7 @@
       <p v-for="channel in channels">
         <nuxt-link :to="`/channels/${channel.id}`">{{ channel.name }}</nuxt-link>
       </p>
+      <p v-if="isAuthenticated" class="logout" v-on:click="logout">ログアウト</p>
     </div>
     <div class="main-content">
       <nuxt />
@@ -13,13 +14,33 @@
 </template>
 
 <script>
-import { db } from "~/plugins/firebase";
+import { db, firebase } from '~/plugins/firebase';
+import { mapActions } from 'vuex';
 
 export default {
   data() {
     return {
       channels: []
     };
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated
+    }
+  },
+  methods: {
+    ...mapActions(['setUser']),
+    logout() {
+      firebase.auth().signOut()
+        .then(() => {
+          this.setUser(null)
+          window.alert('ログアウトに成功！')
+        })
+        .catch((e) => {
+          window.alert('ログアウトに失敗しました')
+          console.log(e)
+        })
+    }
   },
   mounted() {
     db.collection("channels")
@@ -56,4 +77,10 @@ export default {
 
   height: 100vh;
 }
+
+.logout {
+   position: absolute;
+   bottom: 10px;
+   cursor: pointer;
+ }
 </style>
